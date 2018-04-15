b4874c93f0d3dd07482c1ce84f00bbd4c6b6b52a
DESKTOP-54DNJUK\Gavin <gdelima91>
2018-04-14 20:06:44 -0700

    Added fps ray cast, item up / drop

---
+++ Escape Room Game/Assets/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public Material highLightMat;
+
+    private Material oldMat;
+
+	// Use this for initialization
+	void Awake () {
+        oldMat = gameObject.GetComponent<Renderer>().material;
+    }
+
+    public void ActivateHighLight () {
+        gameObject.GetComponent<MeshRenderer>().material = highLightMat;
+    }
+
+    public void DeactivateHighlight() {
+        gameObject.GetComponent<MeshRenderer>().material = oldMat;
+    }
+}
---
+++ Escape Room Game/Assets/PickUp.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : Interactable{
+
+    private bool isPickedUp;
+    private Rigidbody rb;
+    private GameObject holdPos;
+    private Collider _collider;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate() {
+        if (isPickedUp) {
+            SetPickedUpTransform();
+        }
+    }
+
+    public void PickUpObj () {
+        isPickedUp = true;
+
+        if (rb != null) {
+            rb.useGravity = false;
+            //rb.isKinematic = true;
+        }
+
+        if (_collider != null) {
+            _collider.isTrigger = true;
+        }
+    }
+
+    public void DropObj () {
+        isPickedUp = false;
+
+        if (rb != null) {
+            rb.useGravity = true;
+            //rb.isKinematic = false;
+        }
+
+        if (_collider != null) {
+            _collider.isTrigger = false;
+        }
+    }
+
+    void SetPickedUpTransform () {
+        if (holdPos != null) {
+            transform.position = holdPos.transform.position;
+            rb.MovePosition(holdPos.transform.position);
+            transform.rotation = Camera.main.transform.rotation;
+            rb.MoveRotation(Camera.main.transform.rotation);
+        }
+    }
+
+    public bool GetIsPickedUp () {
+        return isPickedUp;
+    }
+
+    public void SetIsPickedUp (bool _isPickedUp) {
+        isPickedUp = _isPickedUp;
+    }
+
+    public void SetHoldPos (GameObject go) {
+        holdPos = go;
+    }
+}

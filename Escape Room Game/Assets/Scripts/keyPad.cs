4bb9ceb449407517a15bb3743cdf02f36eaaf44f
DESKTOP-84KESM8\shigh <shigha25@gmail.com>
2018-05-01 23:22:29 -0500

    fixedTerminalKeypad

--- Escape Room Game/Assets/Scripts/keyPad.cs
+++ Escape Room Game/Assets/Scripts/keyPad.cs
@@ -38,18 +38,18 @@ public class keyPad : MonoBehaviour {
 
             if (keyPadScreen) // keypad and it's input ! problem with input|| it has to be input = input +"" to remember the previous input, but this has a problem with keypads it makes 4 copies of the input
             {
-                if (Input.GetKeyDown(KeyCode.Keypad1))
+                if (Input.GetKeyDown(KeyCode.Keypad7))
                 {
                     //isDone = true;
-                    input = input + "1";
+                    input = input + "7";
                 }
-                if (Input.GetKeyDown(KeyCode.Keypad2))
+                if (Input.GetKeyDown(KeyCode.Keypad8))
                 {
-                    input = input + "2";
+                    input = input + "8";
                 }
-                if (Input.GetKeyDown(KeyCode.Keypad3))
+                if (Input.GetKeyDown(KeyCode.Keypad9))
                 {
-                    input = input + "3";
+                    input = input + "9";
                 }
                 if (Input.GetKeyDown(KeyCode.Keypad4))
                 {
@@ -63,17 +63,17 @@ public class keyPad : MonoBehaviour {
                 {
                     input = input + "6";
                 }
-                if (Input.GetKeyDown(KeyCode.Keypad7))
+                if (Input.GetKeyDown(KeyCode.Keypad1))
                 {
-                    input = input + "7";
+                    input = input + "1";
                 }
-                if (Input.GetKeyDown(KeyCode.Keypad8))
+                if (Input.GetKeyDown(KeyCode.Keypad2))
                 {
-                    input = input + "8";
+                    input = input + "2";
                 }
-                if ( Input.GetKeyDown(KeyCode.Keypad9))
+                if ( Input.GetKeyDown(KeyCode.Keypad3))
                 {
-                    input = input + "9";
+                    input = input + "3";
                 }
                 if (Input.GetKeyDown(KeyCode.Keypad0))
                 {
@@ -87,6 +87,7 @@ public class keyPad : MonoBehaviour {
             openDoor = true;
         }
 
+        //door hinge rotation
         if (openDoor)
         {
             var newRot = Quaternion.RotateTowards(doorHinge.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 250);
@@ -107,18 +108,18 @@ public class keyPad : MonoBehaviour {
 
                 GUI.Box(new Rect(0, 0, 320, 400), "");
                 GUI.Box(new Rect(5, 5, 310, 25), input);
-                if (GUI.Button(new Rect(5, 35, 100, 100), "1"))
+                if (GUI.Button(new Rect(5, 35, 100, 100), "7"))
                 {
                     //isDone = true;
-                    input = input + "1";
+                    input = input + "7";
                 }
-                if (GUI.Button(new Rect(110, 35, 100, 100), "2"))
+                if (GUI.Button(new Rect(110, 35, 100, 100), "8"))
                 {
-                    input = input + "2";
+                    input = input + "8";
                 }
-                if (GUI.Button(new Rect(215, 35, 100, 100), "3"))
+                if (GUI.Button(new Rect(215, 35, 100, 100), "9"))
                 {
-                    input = input + "3";
+                    input = input + "9";
                 }
                 if (GUI.Button(new Rect(5, 140, 100, 100), "4"))
                 {
@@ -132,17 +133,17 @@ public class keyPad : MonoBehaviour {
                 {
                     input = input + "6";
                 }
-                if (GUI.Button(new Rect(5, 240, 100, 100), "7"))
+                if (GUI.Button(new Rect(5, 240, 100, 100), "1"))
                 {
-                    input = input + "7";
+                    input = input + "1";
                 }
-                if (GUI.Button(new Rect(110, 240, 100, 100), "8"))
+                if (GUI.Button(new Rect(110, 240, 100, 100), "2"))
                 {
-                    input = input + "8";
+                    input = input + "2";
                 }
-                if (GUI.Button(new Rect(215, 240, 100, 100), "9"))
+                if (GUI.Button(new Rect(215, 240, 100, 100), "3"))
                 {
-                    input = input + "9";
+                    input = input + "3";
                 }
                 if (GUI.Button(new Rect(110, 350, 100, 100), "0"))
                 {

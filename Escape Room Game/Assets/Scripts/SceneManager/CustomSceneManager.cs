0fdb9e91eae172b7d2a9df8a4051ed25e3d9100b
DESKTOP-54DNJUK\Gavin <gdelima91>
2018-05-13 00:58:54 -0700

    added scene streaming, cut up level into multiple scenes.

---
+++ Escape Room Game/Assets/Scripts/SceneManager/CustomSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomSceneManager : MonoBehaviour {
+    public static CustomSceneManager Instance { set; get; }
+
+    private void Awake() {
+        Instance = this;
+        Load("Player");
+        Load("UI");
+        Load("1");
+    }
+
+    public void Load (string sceneName) {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void Unload (string sceneName) {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.UnloadSceneAsync(sceneName);
+    }
+}
---
+++ Escape Room Game/Assets/Scripts/SceneManager/LoadTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTrigger : MonoBehaviour {
+
+    public string[] loadName;
+    public string[] unloadName;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetComponent<FPSRay>() != null) {
+            Load();
+            StartCoroutine(Unload());
+        }
+    }
+
+    void Load () {
+        foreach (string name in loadName) {
+            CustomSceneManager.Instance.Load(name);
+        }
+    }
+
+    IEnumerator Unload () {
+        yield return new WaitForSeconds(0.1f);
+        foreach (string name in unloadName) {
+            CustomSceneManager.Instance.Unload(name);
+        }
+    }
+}

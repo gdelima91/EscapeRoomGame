ad5d098699bdf0f5ba7f0869baabd5d7db4e8e62
DESKTOP-HQ83206\shigh <shigha25@gmail.com>
2018-05-28 01:27:10 -0500

    added gasmask asset

---
+++ Escape Room Game/Assets/Gas Mask System/PackageAssets/Scripts/HealthController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public float playerHealth = 100.0f;
+    public float healthFall = 2;
+    public float maxHealth = 100.0f;
+
+    [SerializeField] private Text healthUI;
+
+    public static HealthController instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void UpdateHealth()
+    {
+        healthUI.text = playerHealth.ToString("0");
+    }
+
+    public void Death()
+    {
+        //DO SOMETHING HERE!
+        Debug.Log("I DIED"); //Delete this and replace with your own code for dying!
+    }
+}
